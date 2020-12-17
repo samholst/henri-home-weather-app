b056e255494bc0621446e80383f4f44dfa625696
Sam Holst <sawohol@gmail.com>
2020-12-17 12:46:40 -0700

    added zip spec to api spec

--- spec/requests/weather/api_spec.rb
+++ spec/requests/weather/api_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe Weather::Api, type: :request do
   it 'raises an expection if a zip is blank' do
     expect { Weather::Api.new("") }.to raise_error(Exception)
   end
+
+  it 'should have an assigned zip' do
+    weather_api = Weather::Api.new(85339)
+
+    expect(weather_api.instance_variable_get(:@zip)).to be(85339)
+  end
 end
