06de9e149dceb8dc84ead2cc4b0e6ccac0a68795
Sam Holst <sawohol@gmail.com>
2020-12-16 20:41:00 -0700

    added search action logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,20 @@ class PagesController < ApplicationController
   def home
   end
 
+  def search
+    weather_forcast = Weather::Api.new(search_params[:zip_code])
+    result = weather_forcast.get_result
+
+    if result
+      render json: { successful: true, data: result }
+    else
+      render json: { successful: false }
+    end
+  end
+
   private
 
-  def home_params
+  def search_params
     params.permite(:zip_code)
   end
 end
