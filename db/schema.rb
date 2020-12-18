a95abd769ebec07383ce5905df9deb39b6eaaf48
Sam Holst <sawohol@gmail.com>
2020-12-17 19:29:16 -0700

    added mysql2 gem and document on click for class

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_12_17_050250) do
 
-  create_table "locations", force: :cascade do |t|
+  create_table "locations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
